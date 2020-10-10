e499f6b0a243f9234b353a6b790fbf8995986484
headblockhead <26520767+headblockhead@users.noreply.github.com>
2020-10-10 14:59:42 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableNativeFeedbackBase } from 'react-native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TabOne } from './screens/TabOneScreen';
+import { TabTwo } from './screens/TabTwoScreen';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+
+const Tab = createBottomTabNavigator();
+
+const StateContainer = () => {
+  const [count, setCount] = useState(0);
+  const tabOne = () => TabOne(count, () => setCount(count + 1));
+  const tabTwo = () => TabTwo(count);
+  return (
+    <>
+      <Tab.Navigator>
+        <Tab.Screen name="Tab One" component={tabOne} />
+        <Tab.Screen name="Tab Two" component={tabTwo} />
+      </Tab.Navigator>
+    </>
+  )
+};
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +34,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <SafeAreaProvider>
+          <StateContainer />
+          <StatusBar />
+        </SafeAreaProvider>
+      </NavigationContainer>
     );
   }
 }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,27 +1,52 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { Component } from 'react';
+import { Alert, Button, ScrollView, StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+export const TabOne = (count: number, onClick: () => void) => (
+  <ScrollView style={styles.scrollView}>
+    <View style={styles.container} >
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.js" />
+      <Text style={styles.subtitle1}>Welcome to my game!</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Click the button!</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+      <Text>This is a cookie-clicker style game </Text>
+      <Text>where the objective is to collect as many cookies</Text>
+      <Text> as possible</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.big}>{count}</Text>
+
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Button title="llllllllllllllllllllllllllllll" onPress={onClick} />
+
     </View>
-  );
-}
+  </ScrollView>
+);
 
 const styles = StyleSheet.create({
   container: {
+    marginVertical: -45,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  scrollView: {
+
+
+  },
   title: {
     fontSize: 20,
+    fontWeight: 'bold',
+  },
+  big: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  subtitle1: {
+    fontSize: 17,
     fontWeight: 'bold',
   },
   separator: {
@@ -30,3 +55,4 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
